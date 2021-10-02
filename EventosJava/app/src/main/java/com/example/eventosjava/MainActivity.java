9af032e0ff9ecbfbf5cbd7989182ac268f7182b6
avidaldo <avd.formacion@gmail.com>
2021-10-02 20:56:18 +0200

    convirtiendo a expresión lambda equivalente (mejor explicado)

--- EventosJava/app/src/main/java/com/example/eventosjava/MainActivity.java
+++ EventosJava/app/src/main/java/com/example/eventosjava/MainActivity.java
@@ -15,18 +15,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // Implementación usando una clase anónima.
-
         TextView tv = findViewById(R.id.tv);
-        // El escuchador se instancia (new) a partir de una clase anónima
-        tv.setOnClickListener(  // instanciamos un objeto que implementa el interfaz escuchador OnClickListener
-                new View.OnClickListener() { // Implementando directamente los métodos que exige este interfaz:
-                    @Override  // Se sobreescribe el método onClick del escuchador
-                    public void onClick(View v) { // el método recibe como parámetro la View sobre la que se hizo el click (el botón)
-                        Toast.makeText(MainActivity.this, R.string.o3_clase_anonima, Toast.LENGTH_SHORT).show();
-                        // El contexto ya no puede ser this, ya que estamos en la clase anónima
-                    }
+
+
+
+        /*
+        View.OnClickListener es una interfaz funcional (una interfaz que solo tiene un único
+        método abstracto). Desde Java 8, este tipo de interfaces pueden representarse mediante una
+        función lambda.
+
+        Las funciones lambda son funciones anónimas (no tienen un nombre) que pueden ser tratadas como
+        objetos (asignadas a variables o pasadas como parámetros), de este modo, son una herramienta
+        central del paradigma de programación funcional.
+
+        Una función lambda se compopne de sus parámetros de entrada y el código que ejecuta, al igual
+        que cualquier otra función o método, pero sin un nombre.
+
+        Al tener una interfaz funcional un único método, lo relevante de ella son los parámetros
+        que recibe la función que implementa y los parámetros que devuelve.
+
+         */
+
+
+
+        /* El método recibe directamente el método onClick de la clase anónima pero convertido a una
+        función lambda. Esa función tiene el parámetro de entrada v(la view en la que se hizo click)
+        y como salida la acción que realiza ante el evento onClick (el contenido del método onClick) */
+        
+        tv.setOnClickListener(v -> {
+                    Toast.makeText(MainActivity.this, R.string.o3_clase_anonima, Toast.LENGTH_SHORT).show();
                 }
         );
 
